bc231915ab1f2926b77ea650c39200995b417f52
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 18:27:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    newsong = Song.new
-    newsong.name = filename.split(" - ")[1]
-    newsong.artist = filename.split(" - ")[2]
-    newsong
+    
+    @name = filename.split(" - ")[1]
+    @artist = filename.split(" - ")[2]
+    
   end
 
   def artist_name=(name)
