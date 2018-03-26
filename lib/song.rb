44b1b4665a332b1fdcd3832af53c2147e95795d0
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 18:04:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
   attr_accessor :title, :artist
 
+  def initialize(title)
+    @title = title
+  end
+
   def self.new_by_filename(filename)
     song = self.new
     song.title = filename.split(" - ")[1]
