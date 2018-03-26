a5f3cc3fbab7ac2bec39b4c29e8ee03f15a35447
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 19:08:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,13 @@ class Song
     @name
   end
 
-  def self.new_by_filename(filename)
-    new_song = Song.new(filename.split(/\W/)[1])
-    new_song.artist_name = (filename.split(/\W/)[0])
-    new_song
-  end
+  def self.new_by_filename(file)
+     artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+     song = Song.new(song_name)
+     song.artist_name = artist_name
+     song
+   end
 
   def artist_name=(artist_name)
     Artist.find_or_create_by_name(artist_name)
