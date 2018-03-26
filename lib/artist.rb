2a1cc0d818e28b4382a583ea32a36244db2da303
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 19:06:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,14 +17,19 @@ class Artist
     @songs << some_song
   end
 
-  def self.find_or_create_by_name(artist_name)
-      if self.all.detect { |artist| artist.name == name }
-       return self.all.detect { |artist| artist.name == name }
-     else
-       artist = self.new(name)
-       artist.name = name
-       artist      end
-   end
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
 
    def print_songs
      @songs.each { |song| puts song.name }
