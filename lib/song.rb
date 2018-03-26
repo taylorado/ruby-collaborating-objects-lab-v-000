8189d22fc4246e6efcecc6004174e3d173614360
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 18:05:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,12 @@
 class Song
-  attr_accessor :title, :artist
+  attr_accessor :name, :artist
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
   end
 
   def self.new_by_filename(filename)
