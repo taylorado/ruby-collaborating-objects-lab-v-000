02cb8220d28d5238ae6e5750f2e9e209f36a8198
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 18:39:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-     @@all.include? name ? name : @@all << Artist.new(name)
+     @@all.include? name ? Artist.name : @@all << Artist.new(name)
    end
 
    def print_songs
